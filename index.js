780c7b7c13db5651fc5f61b22c8f1eb4c006ebf9
unknown <scotteychaos@gmail.com>
2022-07-28 20:42:23 -0400

    finished

--- index.js
+++ index.js
@@ -5,7 +5,31 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+
+
+async function moveNPC() {
+    await npc.walkNorth(1400)
+    await npc.walkEast(1200)
+    await npc.walkSouth(300)
+    await npc.walkEast(1500)
+    await npc.walkSouth(1500)
+    await npc.walkWest(2700)
+    await npc.walkNorth(400)  
+}
+
+moveNPC()
+
+// async function infiniteLoopWalk() {
+//     await npc.walkEast(5)
+//     await npc.walkNorth(5)
+//     await npc.walkWest(5)
+//     await npc.walkSouth(5)
+//     infiniteLoopWalk()
+// }
+
+// infiniteLoopWalk()
+
+
 
 // Create the inventory
 const inventory = newInventory()
